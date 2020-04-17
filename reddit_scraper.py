2b6930c2756ff0da4a19a4da8a6734755a20e158
Fabian Schmidt <fabis@outlook.com>
2020-04-17 20:02:21 +0200

    fixed subreddit arguments

--- reddit_scraper.py
+++ reddit_scraper.py
@@ -4,6 +4,7 @@ import pandas as pd
 import datetime as dt
 import os
 import sys
+import datetime
 
 class RedditScraper():
 
@@ -22,27 +23,31 @@ class RedditScraper():
                             password=reddit_pw)
 
     def scrape_subreddit(self, subreddit_name):
+        
+        # define subreddit to scrape from
+        subreddit = self.reddit.subreddit(subreddit_name)
 
-        subreddit = self.reddit.subreddit('politics')
-
+        # dictionary with attributes from every submission
         topics_dict = { 
             "title":[],
             "score":[],
             "id":[], "url":[],
             "comms_num": [],
-            "created": [],
+            "created_at": [],
             "body":[]
             }
 
+        # for every submission retrieve and append post attributes to dictionary
         for submission in subreddit.top('day', limit=12):
             topics_dict["id"].append(submission.id)
-            topics_dict["created"].append(submission.created)
+            topics_dict["created_at"].append(submission.created_utc)
             topics_dict["score"].append(submission.score)
             topics_dict["url"].append(submission.url)
             topics_dict["comms_num"].append(submission.num_comments)
             topics_dict["title"].append(submission.title)
             topics_dict["body"].append(submission.selftext)
 
+        # create data frame out of dictionary
         topics_data = pd.DataFrame(topics_dict)
 
         print(topics_data)
@@ -50,9 +55,14 @@ class RedditScraper():
         return topics_data
 
 if __name__ == "__main__":
+
     reddit_scraper = RedditScraper()
+
     if len(sys.argv) > 1:
         subreddit_name = sys.argv[1]
-        reddit_scraper.scrape_subreddit(subreddit_name)
+        print('Scraping subreddit: {}'.format(subreddit_name))
+        output = reddit_scraper.scrape_subreddit(subreddit_name)
     else:
-        reddit_scraper.scrape_subreddit("politics")
+        print('Scraping default subreddit: {}'.format("politics"))
+        output = reddit_scraper.scrape_subreddit("politics")
+        print(datetime.datetime.fromtimestamp(output['created_at'][0]))
