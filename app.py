3d99c67d665950e4f29a457d5f381f62858a4c45
Fabian Schmidt <fabis@outlook.com>
2020-05-30 01:18:28 +0200

    tiles for displaying the summaries

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import numpy as np
 import plotly.express as px
 import pandas as pd
+from summarizer import Summarizer
 
 #df = pd.read_csv('scraped_articles.csv')
 
@@ -10,11 +12,16 @@ df = pd.read_csv('scraped_articles.csv', sep=';')
 
 print(df.text)
 
+#s = Summarizer()
+#test_summary = s.summarize(text = df.text[0])
+test_summary = "Chatbots were the main headliners at the Mobile World Congress 2017. The only question around chatbots was who would monopolize the field, not whether chatbots would take off in the first place. There isn’t even an ecosystem for a platform to dominate. The big divide is text vs. speech."
+
 text_word_count = []
 
 # populate the lists with sentence lengths
 for i in df['text']:
-      text_word_count.append(len(i.split()))
+    if(pd.notna(i)):
+        text_word_count.append(len(i.split()))
 
 length_df = pd.DataFrame({'text':text_word_count})
 
@@ -68,6 +75,81 @@ app.layout = html.Div(
                 ], className= 'six columns'
                 )
             ], className="row"
+            
+        ),
+
+        html.Div(
+            [
+                html.Div(
+                        [
+                            html.Div(
+                                [html.H6(id="tile1_1_text"), html.P(test_summary)],
+                                id="tile1_1",
+                                style={'width': '20%', 'display': 'inline-block'},
+                                className="mini_container",
+                                
+                            ),
+                            html.Div(
+                                [html.H6(id="tile1_2_text"), html.P(test_summary)],
+                                id="tile1_2",
+                                style={'width': '20%', 'display': 'inline-block'},
+                                className="mini_container",
+                            ),
+                            html.Div(
+                                [html.H6(id="tile1_3_text"), html.P(test_summary)],
+                                id="tile1_3",
+                                style={'width': '20%', 'display': 'inline-block'},
+                                className="mini_container",
+                            ),
+                            html.Div(
+                                [html.H6(id="tile1_4_text"), html.P(test_summary)],
+                                id="tile1_4",
+                                style={'width': '20%', 'display': 'inline-block'},
+                                className="mini_container",
+                            ),
+                        ],
+                        id="info-container-1",
+                        className="twelve columns",
+                        ),
+            ], className="row"
+            
+        ),
+
+        html.Div(
+            [
+                html.Div(
+                        [
+                            html.Div(
+                                [html.H6(id="tile2_1_text"), html.P(test_summary)],
+                                id="tile2_1",
+                                style={'width': '20%', 'display': 'inline-block'},
+                                className="mini_container",
+                                
+                            ),
+                            html.Div(
+                                [html.H6(id="tile2_2_text"), html.P(test_summary)],
+                                id="tile2_2",
+                                style={'width': '20%', 'display': 'inline-block'},
+                                className="mini_container",
+                            ),
+                            html.Div(
+                                [html.H6(id="tile2_3_text"), html.P(test_summary)],
+                                id="tile2_3",
+                                style={'width': '20%', 'display': 'inline-block'},
+                                className="mini_container",
+                            ),
+                            html.Div(
+                                [html.H6(id="tile2_4_text"), html.P(test_summary)],
+                                id="tile2_4",
+                                style={'width': '20%', 'display': 'inline-block'},
+                                className="mini_container",
+                            ),
+                        ],
+                        id="info-container-2",
+                        className="twelve columns",
+                        ),
+            ], className="row"
+            
         )
     ], className='ten columns offset-by-one')
 )
